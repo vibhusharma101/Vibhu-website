3c485d6e359f1debbbb5eb02954a4f248fd5e965
vibhusharma101 <sharma.vibhu101@gmail.com>
2025-10-05 09:00:45 +0530

    first commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import { Button, Center } from "@mantine/core";
+import { BlogCard } from "../../components/blog/BlogCard";
+
+export default function Blog() {
+  return (
+    <Center>
+        <BlogCard/>
+    </Center>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { HeroText } from "@/components/hero/HeroText";
+import { Center } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <Center style={{border:"solid red 1px"}}>
+      <HeroText/>
+    </Center>
+  );
+}
